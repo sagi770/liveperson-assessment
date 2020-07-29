2f6b2c1f3ddf7d2fe060d2f24c28a0b0e2fd100d
sagi770 <sagi.770@gmail.com>
2020-07-29 18:44:26 +0300

    Updates

---
+++ precache-manifest.f910165db32d5729233ae612d1ec09f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5f1775905f8e9d9740dfe604de4ac56",
+    "url": "/liveperson-assessment/index.html"
+  },
+  {
+    "revision": "27f684cc1d1224bc2f8e",
+    "url": "/liveperson-assessment/static/js/2.eadad84e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/liveperson-assessment/static/js/2.eadad84e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac8c1a90b38053b07b55",
+    "url": "/liveperson-assessment/static/js/main.867dbb39.chunk.js"
+  },
+  {
+    "revision": "dffee35bffd28129898a",
+    "url": "/liveperson-assessment/static/js/runtime-main.a7f1409a.js"
+  }
+]);
